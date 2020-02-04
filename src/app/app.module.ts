710cb44da1375cc508d2ce9e38c4c271ac5a1a35
Biggo89 <aledev.89@gmail.com>
2020-02-04 15:04:39 +0100

    2 files modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { GameComponent } from './assignment5/game/game.component';
+import { OddComponent } from './assignment5/odd/odd.component';
+import { EvenComponent } from './assignment5/even/even.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, GameComponent, OddComponent, EvenComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
